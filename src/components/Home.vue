6983c932ccc662b19fe2129fc35f0ec32e068942
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-07-21 14:29:59 -0500

    computed functions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,10 @@
     <button @click="estado = !estado">Cambiar valor</button>
   </div>
   <div><button @click="miobj.count++">Aumentar valor</button></div>
+  <div><button @click="proximo = 0">Resetear</button></div>
   <div v-show="estado">¡Apareci!</div>
   <div>{{ miobj.count }}</div>
+  <div>Siguiente valor: {{ proximo }}</div>
 </template>
 
 <script setup>
@@ -13,8 +15,20 @@ import { ref, reactive, watch, computed } from "vue";
 const estado = ref(true);
 const miobj = reactive({ count: 0 });
 
-watch([estado, () => miobj.count], (newValue, oldValue) => {
+// const proximo = computed(() => miobj.count + 1);
+
+const proximo = computed({
+  get: () => miobj.count + 1,
+  set: (valor) => {
+    miobj.count = valor;
+  },
+});
+
+watch(proximo, (newValue, oldValue) => {
   console.log("Anterior", oldValue);
   console.log("Nuevo", newValue);
+  if (miobj.count > 10) {
+    proximo.value = 0;
+  }
 });
 </script>
